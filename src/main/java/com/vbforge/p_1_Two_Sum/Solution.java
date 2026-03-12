3a3dc039b74f85f17ed9f5c58c749c1ffc1a72bc
vbforge <vladbogdantsev@gmail.com>
2026-03-12 14:24:06 +0200

    feat: add automation scripts and CI workflow

--- src/main/java/com/vbforge/p_1_Two_Sum/Solution.java
+++ src/main/java/com/vbforge/p_1_Two_Sum/Solution.java
@@ -23,7 +23,7 @@ public class Solution {
             }
             map.put(nums[i] ,i);
         }
-        throw new IllegalArgumentException("No two sum solution");
+        throw new IllegalArgumentException("No two sum solution available!");
     }
 
 }
